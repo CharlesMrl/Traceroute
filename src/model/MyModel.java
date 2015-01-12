99c39a2957ff30999e73e407bb67d190b93d635d
Charles Mariller <charles.mariller@gmail.com>
2015-01-12 13:57:07 +0100

    Initial commit

---
+++ src/model/MyModel.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package model;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ *
+ * @author charlesmariller
+ */
+public class MyModel {
+    public static void main(String[] args) 
+    {
+        
+        Runtime rt = Runtime.getRuntime();
+        Process process = null;
+        
+        try {
+            process = rt.exec("traceroute google.com");  //execute Memory.java which is in same directory
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+        }
+        
+        InputStream myStream = process.getInputStream();
+
+        int i;
+
+        try {
+            while((i = myStream.read()) != -1)
+                System.out.print((char)i);
+            
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            //e.printStackTrace();
+        }
+
+    }
+}
