847cc211be10d36fd5548adfc6bf93de2ca04bef
Charles Mariller <charles.mariller@gmail.com>
2015-01-26 15:35:10 +0100

    Adding graphStream library

--- src/model/MyModel.java
+++ src/model/MyModel.java
@@ -8,6 +8,11 @@ package model;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import view.GraphView;
 
 /**
  *
@@ -28,17 +33,44 @@ public class MyModel {
         }
         
         InputStream myStream = process.getInputStream();
-
+       
+        
         int i;
-
+        String myString = "";
+        
+        
         try {
             while((i = myStream.read()) != -1)
-                System.out.print((char)i);
+            {
+                myString += (char)i;
+            }
+            System.out.println(myString);
+            String[] test = null;
+            Pattern p = Pattern.compile("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
+            Matcher m = p.matcher(myString);
+            List myTest = new LinkedList();
+            
+            while (m.find())
+            {
+                myTest.add(m.group());
+                System.out.print ("Trouvé < " + m.group());
+                System.out.println (" > de " + m.start() + " à " + m.end());
+            }
+            System.out.println(myTest);
+            /*test = myString.split("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}");
+            
+            for(String elem : test)
+            {
+                System.out.println(elem);
+            }*/
             
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        GraphView graph = new GraphView();
+        graph.testGraph();
+        
 
     }
 }
---
+++ src/view/GraphView.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package view;
+
+
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+/**
+ *
+ * @author charlesmariller
+ */
+public class GraphView {
+   
+    public void testGraph()
+    {
+        org.graphstream.graph.Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.addNode("A" );
+
+
+
+        org.graphstream.graph.Node n = graph.addNode("B");
+        n.addAttribute("ui.label","B");
+        graph.addNode("C" );
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+
+
+
+        graph.addAttribute("ui.stylesheet", "graph { padding: 40px; } node { text-alignment: at-right; text-background-mode: plain; text-background-color: #EB2; text-color: #222; text-size: 20px;}");
+        graph.display();
+    }
+    
+  
+}
